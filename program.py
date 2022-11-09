17deb6606a0b0babafccaceb65306cad9389eb35
GarimaPoonia <106699473+GarimaPoonia@users.noreply.github.com>
2022-11-09 12:53:35 +0530

    Update program.py

--- program.py
+++ program.py
@@ -18,23 +18,25 @@ window.geometry("280x170")
 myFont = tkinter.font.Font(family = 'Helvetica', size = 10, weight = 'bold')
 led = LED(17)
 
-def dot():
+Code = {'A' : '.-' ,'B' : '-...' ,'C' : '-.-.' ,'D' : '-..' ,'E' : '.' ,
+'F' : '..-.' ,'G' : '--.' ,'H' : '....' ,'I' : '..' ,'J' : '.---' ,
+'K' : '-.-' ,'L' : '.-..' ,'M' : '--' ,'N' : '-.' ,'O' : '---' ,
+'P' : '.--.' ,'Q' : '--.-' ,'R' : '.-.' ,'S' : '...' ,'T' : '-' ,
+'U' : '..-' ,'V' : '...-' ,'W' : '.--' ,'X' : '-..-' ,'Y' : '-.--' ,'Z' : '--..'}
+
+def Dash():
+    led.on()
+    time.sleep(0.9)   # the ratio for dot and dash is 1:3
+    led.off()
+    time.sleep(0.3)
+    print("Dash")
+    
+def Dot():
     led.on()
     time.sleep(0.3)
     led.off()
     time.sleep(0.3)
     print("Dot")
-
-def dash():
-	led.on()
-	time.sleep(0.9)       # the ratio for dot and dash is 1:3
-	led.off()
-	time.sleep(0.3)
-    print("Dash")
-
-# closes the current opened window
-def exit():
-    window.destroy()
 
 #takes input from user in form of text and calculates its morse code  
 def MorseCode():
@@ -47,141 +49,19 @@ def MorseCode():
     else:
         print("Calculating morse code...")
         time.sleep(1)
+    for alphabet in text:
+        for dot_dash in Code[alphabet.upper()]:
+            if dot_dash == '-':
+                Dash()
+            elif dot_dash == '.':
+                Dot()
+            else:
+                time.sleep(0.13)
+        time.sleep(1) 
 
-    for keyword in text:    
-        if keyword.lower() == 'a':
-            dot()
-            dash()
-
-        elif keyword.lower() == 'b':
-            dash()
-            dot()
-            dot()
-            dot()
-
-        elif keyword.lower() == 'c':
-            dash()
-            dot()
-            dash()
-            dot()
-
-        elif keyword.lower() == 'd':
-            dash()
-            dot()
-            dot()
-            
-        elif keyword.lower() == 'e':
-            dot()
-            
-        elif keyword.lower() == 'f':
-            dot()
-            dot()
-            dash()
-            dot()
-            
-        elif keyword.lower() == 'g':
-            dash()
-            dash()
-            dot()
-
-        elif keyword.lower() == 'h':
-            dot()
-            dot()
-            dot()
-            dot()
-            
-        elif keyword.lower() == 'i':
-            dot()
-            dot()
-
-        elif keyword.lower() == 'j':
-            dot()
-            dash()
-            dash()
-            dash()
-            
-        elif keyword.lower() == 'k':
-            dash()
-            dot()
-            dash()
-            
-        elif keyword.lower() == 'l':
-            dot()
-            dash()
-            dot()
-            dot()
-            
-        elif keyword.lower() == 'm':
-            dash()
-            dash()
-
-        elif keyword.lower() == 'n':
-            dash()
-            dot()
-            
-        elif keyword.lower() == 'o':
-            dash()
-            dash()
-            dash()
-            
-        elif keyword.lower() == 'p':
-            dot()
-            dash()
-            dash()
-            dot()
-            
-        elif keyword.lower() == 'q':
-            dash()
-            dash()
-            dot()
-            dash()
-            
-        elif keyword.lower() == 'r':
-            dot()
-            dash()
-            dot()
-            
-        elif keyword.lower() == 's':
-            dot()
-            dot()
-            dot()
-            
-        elif keyword.lower() == 't':
-            dash()
-            
-        elif keyword.lower() == 'u':
-            dot()
-            dot()
-            dash()
-            
-        elif keyword.lower() == 'v':
-            dot()
-            dot()
-            dot()
-            dash()
-            
-        elif keyword.lower() == 'w':
-            dot()
-            dash()
-            dash()
-            
-        elif keyword.lower() == 'x':
-            dash()
-            dot()
-            dot()
-            dash()
-            
-        elif keyword.lower() == 'y':
-            dash()
-            dot()
-            dash()
-            dash()
-            
-        elif keyword.lower() == 'z':
-            dash()
-            dash()
-            dot()
-            dot()
+# closes the current opened window
+def exit():
+    window.destroy()
 
 Label(window, text = 'Enter name', bg = 'bisque2', font = myFont, height = 1, width = 35).grid(row = 0, column = 1)
 
